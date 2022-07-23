3eb2c090846910511767b566d23428f063bb90d3
Haris Agic <agic@ntara.com>
2022-07-23 22:21:20 +0200

    Add Entity Frameword Core and View Inventory Use Case

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,7 +1,14 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
+        public int InventoryId { get; set; }
 
+        [Required]
+        public string? InventoryName { get; set; } // this something nwq in C# 10
+        public int Quantity { get; set; }
+        public double Price { get; set; }
     }
 }
